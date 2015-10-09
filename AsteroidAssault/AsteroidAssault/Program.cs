680081eab9860a7a2a82de476322170d1b93e4da
Kyle Herve <g00243434@W213-08.academic.raritanval.edu>
2015-10-09 12:37:08 -0400

    It Works

--- AsteroidAssault/AsteroidAssault/Program.cs
+++ AsteroidAssault/AsteroidAssault/Program.cs
@@ -1,13 +1,17 @@
 using System;
 
+#if MONOMAC
+using MonoMac.Foundation;
+using MonoMac.AppKit;
+using MonoMac.ObjCRuntime;
+#endif
+
 namespace AsteroidAssault
 {
-#if WINDOWS || XBOX
+#if WINDOWS || XBOX || LINUX
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+
         static void Main(string[] args)
         {
             using (Game1 game = new Game1())
@@ -16,6 +20,36 @@ namespace AsteroidAssault
             }
         }
     }
+#elif MONOMAC
+	static class Program
+	{
+
+		static void Main (string[] args)
+		{
+			NSApplication.Init ();
+			
+			using (var p = new NSAutoreleasePool ()) {
+				NSApplication.SharedApplication.Delegate = new AppDelegate();
+				NSApplication.Main(args);
+			}
+		}
+	}
+	
+	class AppDelegate : NSApplicationDelegate
+	{
+		Game1 game;
+		public override void FinishedLaunching (MonoMac.Foundation.NSObject notification)
+		{
+			game = new Game1();
+			game.Run();
+		}
+		
+		public override bool ApplicationShouldTerminateAfterLastWindowClosed (NSApplication sender)
+		{
+			return true;
+		}
+	}		
+#else
+#error Unknown platform
 #endif
 }
-
